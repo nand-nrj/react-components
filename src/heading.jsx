04a1859cdf1f08d910c7f7a5352e1b3e95b79a12
nand-nrj <76772181+nand-nrj@users.noreply.github.com>
2022-03-05 12:58:12 +0530

    Initial commit

---
+++ src/heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  return <h1>My Favourite Fruits</h1>;
+}
+
+export default Heading;
---
+++ src/list.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>apple</li>
+      <li>mango</li>
+      <li>banana</li>
+    </ul>
+  );
+}
+
+export default List;
